40c99138df0a0143296bd8ed7c9bf67cbe126cc3
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-01-18 18:34:59 -0800

    cmp - clean up

--- src/veux/reader/__main__.py
+++
@@ -1,8 +0,0 @@
-import veux.reader.csi as csi
-
-if __name__ == "__main__":
-    import sys
-    with open(sys.argv[1], "r") as f:
-        tab = csi.load(f)
-
-    csi.create_model(tab, verbose=True)
