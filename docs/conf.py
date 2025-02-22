466e5836171ae30b6bf29b7de800b363cdc8303a
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-02-22 09:39:51 -0800

    cmp

--- docs/conf.py
+++ docs/conf.py
@@ -86,8 +86,8 @@ def _add_examples(app, pagename, templatename, context, doctree):
             {"title": "Frames",      "link": f"{g}/examples/example5/",     "image": "../_static/images/gallery/Example5.png", "description": "Render structural models with extruded sections."},
             {"title": "Sections",    "link": f"{g}/examples/framesections/",     "image": "../_static/images/gallery/Torsion.png", "description": "Detailed analysis of structural cross sections."},
             {"title": "Detailing",   "link": f"{g}/examples/example7/",     "image": "../_static/images/gallery/ShellFrame.png", "description": "."},
-            {"title": "Motions",     "link": f"{g}/examples/framecircle/",  "image": "../_static/images/gallery/ShellCircle-576x324.webp", "description": "Coming soon."},
-            {"title": "Interoperability", "link": f"{g}/examples/cablestayed/",  "image": "../_static/images/gallery/CableStayed02-576x324.webp", "description": "Coming soon."},
+            {"title": "Finite Rotations",  "link": f"{g}/examples/framecircle/",  "image": "../_static/images/gallery/ShellCircle-576x324.webp", "description": "Render finite deformations in constrained members like Cosserat rods and shells."},
+            {"title": "Interoperable",     "link": f"{g}/examples/cablestayed/",  "image": "../_static/images/gallery/CableStayed02-576x324.webp", "description": "Import models from commercial platforms like ABAQUS."},
 #           {"title": "Motions",     "link": f"{g}/examples/framehelix/",  "image": "../_static/images/gallery/sign-light-2800x2558.webp", "description": "Coming soon."},
 #           {"title": "Interoperability", "link": f"{g}/examples/cablestayed/",  "image": "../_static/images/gallery/CableStayed02-576x324.webp", "description": "Coming soon."},
         ]
