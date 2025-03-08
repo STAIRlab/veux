506d43be32578a6fe1ce7490dd0271b7ab46de64
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-03-07 16:14:37 -0800

    Use new Server constructor

--- src/veux/viewer/__main__.py
+++ src/veux/viewer/__main__.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     with open(filename, "rb") as f:
         glb = f.read()
 
-    Server(glb=glb, **options).run()
+
+    Server(viewer=Viewer(glb, **options)).run()
 
 
