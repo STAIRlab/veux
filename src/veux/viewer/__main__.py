8349e301e1129990ebc6ec07b8e67b099ae5e549
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-22 08:38:35 -0700

    Update __main__.py

--- src/veux/viewer/__main__.py
+++ src/veux/viewer/__main__.py
@@ -4,14 +4,20 @@ import sys
 import bottle
 from veux.viewer import Viewer
 from veux.server import Server
+from argparse import ArgumentParser
 
 
 if __name__ == "__main__":
 
+    parser = ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("--viewer", default="mv", help="Viewer to use (default: mv)")
+    args = parser.parse_args()
+
     options = {
-        "viewer": "mv" #"three-170"
+        "viewer": args.viewer
     }
-    filename = sys.argv[1]
+    filename = args.filename
 
     with open(filename, "rb") as f:
         glb = f.read()
